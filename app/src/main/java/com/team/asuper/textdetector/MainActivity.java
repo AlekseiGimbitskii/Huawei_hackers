099afc4976bf177761d71557d30780ef81a0163b
AlekseiGimbitskii <aleksei.gimbitskii@student.tut.fi>
2018-12-17 18:21:38 +0200

    text rcognition works from image

--- app/src/main/java/com/team/asuper/textdetector/MainActivity.java
+++ app/src/main/java/com/team/asuper/textdetector/MainActivity.java
@@ -1,11 +1,15 @@
 package com.team.asuper.textdetector;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
@@ -17,6 +21,7 @@ import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
 import java.io.FileInputStream;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +31,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        Bitmap bitmap = BitmapFactory.decodeFile("textarea.png");
+
+    }
+
+    public void detectText(View view) {
+        // Do something in response to button
+
+        //---------------read file, detect text and display it on textview--------------------------
+
+        final TextView textview = (TextView) findViewById(R.id.screenText);
+        //textview.setText("success");
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.textarea);
+        //Drawable drawable = new BitmapDrawable(getResources(), bitmap);
+        //textview.setBackground(drawable);
+
 
         FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
         FirebaseVisionTextRecognizer detector = FirebaseVision.getInstance().getOnDeviceTextRecognizer();
@@ -36,22 +54,32 @@ public class MainActivity extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
                     @Override
                     public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                        //textview.setText("success");
+                        String resultText = firebaseVisionText.getText();
+                        textview.setText(resultText);
 
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-
+                        textview.setText("failure");
                     }
                 });
 
 
+/*
+        try {
         String resultText = result.getResult().getText();
-        Log.d("tah", resultText);
+        //Log.d("tah", resultText)
+        }
+        catch (Exception e) {
+            textview.setText(e.getMessage());
+        }
 
-        TextView view = (TextView) findViewById(R.id.screenText);
-        view.setText(resultText);
+
+        //textview.setText(resultText);
+*/
+
     }
-
 }
