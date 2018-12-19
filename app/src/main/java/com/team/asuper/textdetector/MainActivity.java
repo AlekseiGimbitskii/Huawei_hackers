2447791b0296fc7e477060391d3ed857da197c4d
MD <example@malexample.com>
2018-12-19 21:42:17 +0200

    Added toolbar

--- app/src/main/java/com/team/asuper/textdetector/MainActivity.java
+++ app/src/main/java/com/team/asuper/textdetector/MainActivity.java
@@ -5,7 +5,9 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -44,6 +46,10 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
         mDrawerLayout = (DrawerLayout) findViewById(R.id.activity_main);
         NavigationView navigationView = findViewById(R.id.nav_view);
 
@@ -70,6 +76,14 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     /*
     public void detectText(View view) {
